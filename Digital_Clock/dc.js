32a7174c259623ee710879fafbf33b797dcc264f
rudra-1157 <rudraprasadpadhy202@gmail.com>
2026-08-15 16:08:27 +0530

    Updated

--- Digital_Clock/dc.js
+++ Digital_Clock/dc.js
@@ -1,8 +1,9 @@
 function updateClock() {
     const now = new Date();
-    let hours = now.getHours().toString().padStart(2, '0');
+    let hours = now.getHours();
     const amPm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
     const timeString = `${hours}:${minutes}:${seconds} ${amPm}`;
